81ba3b0fec6a88b2f42cdc81fad0d0ee3b0f02bf
rmag4233 <ryan.agnew@libertymutual.com>
2017-11-26 19:06:07 -0500

    Adds create gears functionality
    Adds functionality to create a gear. Need to check on the message being
    consoled as there seems to be some issue.

--- src/app/gears/gear-new/gear-new.component.ts
+++ src/app/gears/gear-new/gear-new.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GearsService } from '../gears.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gear-new',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GearNewComponent implements OnInit {
 
-  constructor() { }
+  newGear = <any>{};
+
+  constructor(
+    private gearsService : GearsService,
+    private router : Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  saveGear(newGear) {
+  	console.log(this.newGear.title);
+  	this.gearsService.saveGear(this.newGear.title, this.newGear.description, this.newGear.price, this.newGear.availability, this.newGear.image_URL)
+  			.subscribe(response => {
+			console.log(response.json());
+			let gear = response.json();
+			this.router.navigate(["/gear/" + gear.gear.id]);
+		})
   }
 
 }
--- src/app/gears/gears.service.ts
+++ src/app/gears/gears.service.ts
@@ -6,8 +6,21 @@ import { environment } from '../../environments/environment';
 export class GearsService {
 
   getOneGear(gearId) {
-		console.log(gearId);
 		return this.http.get(environment.apiServer + '/gears/' + gearId);
+	}
+
+  saveGear(title: string, description: string, price: string, availability: boolean, image_URL: string) {
+		console.log('title being sent to api is ', title);
+    let gear = {
+      'gear': {
+        'title': title,
+        'description': description,
+        'price': price,
+        'availability': availability,
+        'image_URL': image_URL
+      }
+    }
+		return this.http.post(environment.apiServer + '/gears', gear);
 	}
 
   constructor(private http: Http) { }
