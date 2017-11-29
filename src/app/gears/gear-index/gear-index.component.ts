9f71a241bc3c93097a965e3ad854179e919d9cd6
rmag4233 <ryan.agnew@libertymutual.com>
2017-11-29 16:32:14 -0500

    Adds a search bar to the gears index page
    Adds search bar to gears index page, need to get search function
    running

--- src/app/gears/gear-index/gear-index.component.ts
+++ src/app/gears/gear-index/gear-index.component.ts
@@ -10,6 +10,7 @@ export class GearIndexComponent implements OnInit {
 
   allGears = [];
   error: any;
+  searchedGear = [];
 
   deleteGear(deletedGear) {
     this.gearsService.deleteGear(deletedGear)
@@ -19,6 +20,10 @@ export class GearIndexComponent implements OnInit {
       this.error = null
     },
     err => this.error = err);
+  }
+
+  searchGear(search) {
+    console.log('search is ', search)
   }
 
   constructor(private gearsService : GearsService) { }
